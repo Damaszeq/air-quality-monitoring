bae65cbb5f0a72ded749393e926c4b68eff318b6
Damaszeq <tomasz.wejner@student.put.poznan.pl>
2025-04-02 16:26:28 +0200

    Initial project structure

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "main.h"
+
+int main() {
+    std::cout << "Air Quality Monitoring Application" << std::endl;
+    // Logika aplikacji (na razie tylko test)
+    return 0;
+}
+
